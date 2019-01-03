d622b2871cb174156f33e3079aff0534cbe33c52
ChrisLR <arzhul@gmail.com>
2019-01-03 09:10:20 -0500

    WIP on factory/objectpool

--- pygrunner/gamedata/factory.py
+++ pygrunner/gamedata/factory.py
@@ -1,18 +1,40 @@
+
+
+
 class Factory(object):
     """
     The factory spawns game objects in the game space.
     It also uses a pool to help with garbage collection
     """
-    def __init__(self, sprite_loader):
+    known_recipes = {}
+
+    def __init__(self, sprite_loader, object_pool):
+
         self.sprite_loader = sprite_loader
+        self.object_pool = object_pool
+
+    def get_or_create(self, recipe, location):
+        recipe = self._get_recipe(recipe)
+        game_object = self.object_pool.get(recipe)
+        if game_object:
+            game_object.location = location
+            return game_object
+        else:
+            game_object = recipe.create(location, self.sprite_loader)
+            return game_object
+
+    def _get_recipe(self, recipe):
+        if isinstance(recipe, str):
+            recipe = self.known_recipes.get(recipe)
+            if recipe is None:
+                raise Exception("Recipe '%s' not known." % recipe)
+        else:
+            if hasattr(recipe, 'create') and hasattr(recipe, 'reset'):
+                return recipe
+
+        raise Exception("Recipe '%s' is not valid.")
 
 
-    def _set_animations(self, spriteloader):
-        frames = []
-        for sheet_name, name, row, col in (self.frame_0_opts, self.frame_1_opts):
-            sheet = spriteloader.get_by_name(sheet_name)
-            sheet.set_region_name(name, row, col)
-            frame = pyglet.image.AnimationFrame(sheet.get_region_by_name(name), 0.1)
-            frames.append(frame)
-
-        return pyglet.image.Animation(frames)
+    @classmethod
+    def register(cls, recipe):
+        cls.known_recipes[recipe.name] = recipe
--- pygrunner/gamedata/objectpool.py
+++ pygrunner/gamedata/objectpool.py
@@ -2,3 +2,37 @@ class ObjectPool(object):
     """
     This pool stores created objects and retrieves them
     """
+    def __init__(self):
+        self._pool = {}
+
+
+    def add(self, recipe, game_object):
+        """
+        Adds a game object to the pool
+        :param recipe:
+        :param game_object:
+        :return:
+        """
+        if recipe in self._pool:
+            self._pool[recipe].append(game_object)
+        else:
+            self._pool[recipe] = [game_object]
+
+
+    def get(self, recipe):
+        """
+        Get a game object from the pool and reinitialize it.
+        :param recipe: The recipe associated to the game object
+        :return: An initialized game object
+        """
+        if recipe not in self._pool:
+            return None
+
+        game_object_list = self._pool[recipe]
+        if not game_object_list:
+            return None
+
+        game_object = game_object_list.pop(0)
+        recipe.reset(game_object)
+
+        return game_object
