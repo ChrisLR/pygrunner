670d6667067de70f1121436062a76895f9ff7bbb
ChrisLR <arzhul@gmail.com>
2019-01-06 19:18:17 -0500

    Added a second character

--- pygrunner/gamedata/recipes/characters/__init__.py
+++ pygrunner/gamedata/recipes/characters/__init__.py
@@ -1,1 +1,1 @@
-from pygrunner.gamedata.recipes.characters.humans import HumanMale1
+from pygrunner.gamedata.recipes.characters.humans import HumanMale1, HumanFemale1
--- pygrunner/gamedata/recipes/characters/humans.py
+++ pygrunner/gamedata/recipes/characters/humans.py
@@ -22,3 +22,24 @@ class HumanMale1(Character):
         ],
         'dead': [SpriteInfo('packed', 'human_male_1_dead_0', 30, 18)],
     }
+
+
+@Factory.register
+class HumanFemale1(Character):
+    name = "Human Male 1"
+    animations = {
+        'idle': [SpriteInfo('packed', 'human_female_1_idle_0', 29, 12)],
+        'run': [
+            SpriteInfo('packed', 'human_female_1_run_0', 29, 13),
+            SpriteInfo('packed', 'human_female_1_run_1', 29, 14),
+        ],
+        'climb':[
+            SpriteInfo('packed', 'human_female_1_climb_0', 29, 15),
+            SpriteInfo('packed', 'human_female_1_climb_1', 29, 16, True),
+        ],
+        'punch': [
+            SpriteInfo('packed', 'human_female_1_punch_0', 29, 15),
+            SpriteInfo('packed', 'human_female_1_punch_1', 29, 17),
+        ],
+        'dead': [SpriteInfo('packed', 'human_female_1_dead_0', 29, 18)],
+    }
