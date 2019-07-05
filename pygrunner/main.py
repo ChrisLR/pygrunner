bc0ec52ecaaa94575ac84a18fda961ed8caa09e5
ChrisLR <arzhul@gmail.com>
2019-07-05 12:40:30 -0400

    Remove leftovers of profiling

--- pygrunner/main.py
+++ pygrunner/main.py
@@ -1,10 +1,8 @@
 from pygrunner.client.clientgame import ClientGame
 from pygrunner.client.scenes.manager import SceneManager
-import cProfile
 
 
 if __name__ == '__main__':
     manager = SceneManager()
     game = ClientGame(manager)
     game.start()
-    #cProfile.run('game.start()')
