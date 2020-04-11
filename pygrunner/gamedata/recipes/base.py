7b5e809d035f87e8fdacaf1e050be020d0a37fb4
ChrisLR <arzhul@gmail.com>
2020-04-11 11:06:42 -0400

    Objects with only one Frame will now use the image instead of the Animation object for performance reasons

--- pygrunner/gamedata/recipes/base.py
+++ pygrunner/gamedata/recipes/base.py
@@ -38,12 +38,17 @@ class Recipe(metaclass=abc.ABCMeta):
     def _set_animations(cls, sprite_loader):
         animations = {}
         for animation_name, sprite_infos in cls.animations.items():
-            frames = []
+            images = []
             for sprite_info in sprite_infos:
                 sheet = sprite_loader.get_by_name(sprite_info.spritesheet_name)
                 sheet.set_region_name(sprite_info.sprite_name, sprite_info.row, sprite_info.column)
-                frame = pyglet.image.AnimationFrame(sheet.get_region_by_name(sprite_info.sprite_name), 0.1)
-                frames.append(frame)
-            animations[animation_name] = pyglet.image.Animation(frames)
+                image = sheet.get_region_by_name(sprite_info.sprite_name)
+                images.append(image)
+
+            if len(images) > 1:
+                frames = [pyglet.image.AnimationFrame(image, 0.1) for image in images]
+                animations[animation_name] = pyglet.image.Animation(frames)
+            else:
+                animations[animation_name] = images[0]
 
         return animations
