c02bf3cc497ea66da379d081c94c0aae56b55f6f
ChrisLR <arzhul@gmail.com>
2020-07-01 16:58:40 -0400

    Specify grid_size in game options

--- pygrunner/main.py
+++ pygrunner/main.py
@@ -4,7 +4,7 @@ from pyg2d.core.game import GameContext
 from pygrunner.gamedata.recipes.characters import humans
 
 if __name__ == '__main__':
-    options = GameOptions("sprites", "tmx", [humans.HumanMale1])
+    options = GameOptions("sprites", "tmx", [humans.HumanMale1], grid_size=32)
     game_context = GameContext(options)
     game_context.sprite_loader.load_grid_sheet("sidescroller/packed.png", grid_size=32, name="packed")
     game = GameManager(game_context)
